c72c8d4ed7268aa505454b79394d95511f8205fa
326167Dian <ahmadnurdiansyah81@gmail.com>
2026-05-09 02:41:31 +0700

    koreksi koneksi

--- configurasi/koneksi.php
+++ configurasi/koneksi.php
@@ -1,25 +1,9 @@
 <?php
-// Date_Default_timezone_set('Asia/jakarta');
-// $server = "localhost";
-// $user = "u725913413_bob";
-// $password = "7390091979Dian&&";
-// $database = "u725913413_d71kd";
-// set_time_limit(1800);
-
-// try {
-//     $dsn = "mysql:host=$server;dbname=$database;charset=utf8";
-//     $db = new PDO($dsn, $user, $password);
-//     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-// } catch (PDOException $e) {
-//     die("Koneksi gagal: " . $e->getMessage());
-// }
-
-
+Date_Default_timezone_set('Asia/jakarta');
 $server = "localhost";
-$user = "root";
-$password = "";
-$database = "rme";
+$user = "u725913413_bob";
+$password = "7390091979Dian&&";
+$database = "u725913413_d71kd";
 set_time_limit(1800);
 
 try {
@@ -30,3 +14,19 @@ try {
 } catch (PDOException $e) {
     die("Koneksi gagal: " . $e->getMessage());
 }
+
+
+// $server = "localhost";
+// $user = "root";
+// $password = "";
+// $database = "rme";
+// set_time_limit(1800);
+
+// try {
+//     $dsn = "mysql:host=$server;dbname=$database;charset=utf8";
+//     $db = new PDO($dsn, $user, $password);
+//     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+// } catch (PDOException $e) {
+//     die("Koneksi gagal: " . $e->getMessage());
+// }
